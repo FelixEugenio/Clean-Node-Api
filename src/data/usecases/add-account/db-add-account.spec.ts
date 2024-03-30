845644023a15abc713366e8fc4934dcdc9a51085
felix mavila <felixmacarenco@gmail.com>
2024-03-30 18:21:52 +0000

    test: ensure DbAddAccount returns an account or sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -85,4 +85,22 @@ describe('DbAddAccount Usecase',()=>{
             password:'hashed_password'
         })
     })
+
+    test('Should return an Account on sucess',async()=>{
+        const {sut} = makeSut()
+       
+        const accountData = {
+            name:'valid_name',
+            email:'valid_email@mail.com',
+            password:'valid_password'
+        }
+     const account =   await sut.add(accountData)
+        expect(account).toEqual({
+            id:'valid_id',
+            name:'valid_name',
+            email:'valid_email@mail.com',
+            password:'hashed_password'
+        })
+    })
+
 })
